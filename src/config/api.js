2877a5925c1869b88c5fdbc3072fd6770521f939
JiChuangWei <2323176669@qq.com>
2017-11-16 10:24:30 +0800

    data.js

--- src/config/api.js
+++ src/config/api.js
@@ -1,20 +1,19 @@
 import sfetch from 'sfetch'
-
-
+import {data} from './data.js'
 export const getData = () => {
-	let url = `data.json`;
+	// let url = `data.json`;
 	return new Promise((resolve, reject) => {
-		sfetch.get({
-			url: url,
-			timeout: 12000,
-		}).then((data) => {
-			if (data.ok) {
-				resolve(data.json)
-			} else {
-				reject(data)
-			}
-		});
-
+		resolve(data);
+		// sfetch.get({
+		// 	url: url,
+		// 	timeout: 12000,
+		// }).then((data) => {
+		// 	if (data.ok) {
+		// 		resolve(data.json)
+		// 	} else {
+		// 		reject(data)
+		// 	}
+		// });
 	});
 }
 
